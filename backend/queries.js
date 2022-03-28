61079ea49a2a67ab365883f571bd020db30d792c
Jorge De La Mora <jorgedlmv@gmail.com>
2022-03-28 11:55:37 -0600

    Environment ready for interview

--- backend/queries.js
+++ backend/queries.js
@@ -1,19 +1,8 @@
-const insertMovieReview = (movieTitle, description) => {
-    return `
-        INSERT INTO density_labs_db.movie_reviews (id, name, description) 
-        VALUES(DEFAULT, '${movieTitle}', '${description}')`;
-};
 
-const getMovieRewiew = (movieTitle) => {
-    return `SELECT * FROM density_labs_db.movie_reviews WHERE name = '${movieTitle}'`;
-};
-
-const getAllMovieReviews = () => {
-    return `SELECT * FROM density_labs_db.movie_reviews`;
+const getTestTable = () => {
+    return `SELECT * FROM density_labs_db.test`;
 };
 
 module.exports = {
-    insertMovieReview,
-    getMovieRewiew,
-    getAllMovieReviews
+    getTestTable
 }
